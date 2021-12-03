b081460205c3fe7fc560014715cd5ebae31094fc
Pieter van Loon <pharmit@pietervanloon.com>
2021-12-03 08:36:19 +0100

    Added day3

---
+++ day3/day3.dart
@@ -0,0 +1,64 @@
+import '../common.dart';
+
+final one = '1'.codeUnitAt(0);
+
+List<int> parseLine(String line) {
+  return [
+    for (final c in line.split("")) int.parse(c),
+  ];
+}
+
+final part1 = Part(
+  parser: parseLine,
+  implementation: (input) {
+    final result = List<int>.filled(input[0].length, 0);
+    for (final number in input) {
+      for (int i = 0; i < number.length; i++) {
+        result[i] += number[i];
+      }
+    }
+    var gammaString = '';
+    var espilonString = '';
+    final halfInputLength = input.length ~/ 2;
+    for (final count in result) {
+      gammaString += count > halfInputLength ? '1' : '0';
+      espilonString += count > halfInputLength ? '0' : '1';
+    }
+    final gamma = int.parse(gammaString, radix: 2);
+    final espilon = int.parse(espilonString, radix: 2);
+    print('gamma: $gamma, espilon: $espilon');
+    return (gamma * espilon).toString();
+  },
+);
+
+final part2 = Part(
+  parser: parseLine,
+  implementation: (input) {
+    var oxygenNumbers = List.of(input);
+    for (int i = 0; i < input.first.length; i++) {
+      if (oxygenNumbers.length <= 1) break;
+      final halfLength = oxygenNumbers.length / 2;
+      int count = 0;
+      for (int j = 0; j < oxygenNumbers.length; j++) {
+        count += oxygenNumbers[j][i];
+      }
+      final mask = count >= halfLength ? 1 : 0;
+      oxygenNumbers = oxygenNumbers.where((n) => n[i] == mask).toList();
+    }
+    var co2Numbers = List.of(input);
+    for (int i = 0; i < input.first.length; i++) {
+      if (co2Numbers.length <= 1) break;
+      final halfLength = co2Numbers.length / 2;
+      int count = 0;
+      for (int j = 0; j < co2Numbers.length; j++) {
+        count += co2Numbers[j][i];
+      }
+      final mask = count >= halfLength ? 0 : 1;
+      co2Numbers = co2Numbers.where((n) => n[i] == mask).toList();
+    }
+    final oxygen = int.parse(oxygenNumbers.first.join(), radix: 2);
+    final co2 = int.parse(co2Numbers.first.join(), radix: 2);
+    print('oxygen: $oxygen, co2: $co2');
+    return (oxygen * co2).toString();
+  },
+);
--- main.dart
+++ main.dart
@@ -3,6 +3,7 @@
 import 'dart:io';
 import 'day1/day1.dart' as day1;
 import 'day2/day2.dart' as day2;
+import 'day3/day3.dart' as day3;
 
 void main(List<String> args) {
   if (args.length < 3) throw 'Not enough arguments';
@@ -10,6 +11,7 @@ void main(List<String> args) {
   final days = {
     'day1': [day1.part1, day1.part2],
     'day2': [day2.part1, day2.part2],
+    'day3': [day3.part1, day3.part2],
   };
   final part = int.tryParse(args[1].replaceFirst('part', ''));
   if (part == null || part == 0 || part > 2)

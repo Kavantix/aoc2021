e6c8c43ab88a8d030115ff23426d83ecf3c09252
Pieter van Loon <pharmit@pietervanloon.com>
2021-12-05 10:04:05 +0100

    Condensed day5

--- day5/day5.dart
+++ day5/day5.dart
@@ -1,22 +1,19 @@
-import 'dart:math';
-
 import '../common.dart';
 
 class Point {
   const Point(this.x, this.y);
-  final int x;
-  final int y;
+  final int x, y;
 
-  @override
-  String toString() {
-    return '($x, $y)';
+  static Point fromString(String input) {
+    final parts = input.split(',').map(int.parse).toList();
+    return Point(parts[0], parts[1]);
   }
 }
 
 class LineIterator extends Iterator<Point> {
   LineIterator(this.line)
-      : _dx = line.end.x.compareTo(line.start.x),
-        _dy = line.end.y.compareTo(line.start.y);
+      : _dx = line.to.x.compareTo(line.from.x),
+        _dy = line.to.y.compareTo(line.from.y);
 
   final Line line;
   Point? _current;
@@ -30,10 +27,10 @@ class LineIterator extends Iterator<Point> {
   bool moveNext() {
     final current = _current;
     if (current == null) {
-      _current = line.start;
+      _current = line.from;
       return true;
     }
-    if (current.x == line.end.x && current.y == line.end.y) {
+    if (current.x == line.to.x && current.y == line.to.y) {
       return false;
     }
     _current = Point(current.x + _dx, current.y + _dy);
@@ -42,65 +39,37 @@ class LineIterator extends Iterator<Point> {
 }
 
 class Line extends Iterable<Point> {
-  Line({
-    required this.start,
-    required this.end,
-  });
-  final Point start;
-  final Point end;
+  Line({required this.from, required this.to});
+  final Point from, to;
 
-  bool get isHorizontal => start.y == end.y;
-  bool get isVertical => start.x == end.x;
-  bool get isDiagonal => start.x != end.x && start.y != end.y;
+  bool get isHorizontal => from.y == to.y;
+  bool get isVertical => from.x == to.x;
+  bool get isDiagonal => !isHorizontal && !isVertical;
 
   static Line fromString(String input) {
     final parts = input.split(' -> ');
-    final start = parts[0].split(',').map(int.parse).toList();
-    final end = parts[1].split(',').map(int.parse).toList();
     return Line(
-      start: Point(start[0], start[1]),
-      end: Point(end[0], end[1]),
+      from: Point.fromString(parts[0]),
+      to: Point.fromString(parts[1]),
     );
   }
 
   @override
   LineIterator get iterator => LineIterator(this);
-
-  @override
-  String toString() {
-    return '${start.x},${start.y} -> ${end.x},${end.y}';
-  }
 }
 
-List<Line> parseLines(List<String> input) {
-  return input.map(Line.fromString).toList();
-}
+Part<Iterable<Line>> partWhere(bool Function(Line line) test) => Part(
+      parser: (input) => input.map(Line.fromString).where(test),
+      implementation: (lines) {
+        final board = List.generate(1000, (_) => List.filled(1000, 0));
+        for (final line in lines) {
+          for (final point in line) {
+            board[point.y][point.x] += 1;
+          }
+        }
+        return board.expand((l) => l).where((c) => c > 1).length.toString();
+      },
+    );
 
-final part1 = Part(
-  parser: parseLines,
-  implementation: (lines) {
-    final board = List.generate(1000, (_) => List.filled(1000, 0));
-    for (final line
-        in lines.where((line) => line.isHorizontal || line.isVertical)) {
-      for (final point in line) {
-        board[point.y][point.x] += 1;
-      }
-    }
-    final overlaps = board.expand((l) => l).where((c) => c > 1).length;
-    return overlaps.toString();
-  },
-);
-
-final part2 = Part(
-  parser: parseLines,
-  implementation: (lines) {
-    final board = List.generate(1000, (_) => List.filled(1000, 0));
-    for (final line in lines) {
-      for (final point in line) {
-        board[point.y][point.x] += 1;
-      }
-    }
-    final overlaps = board.expand((l) => l).where((c) => c > 1).length;
-    return overlaps.toString();
-  },
-);
+final part1 = partWhere((l) => l.isHorizontal || l.isVertical);
+final part2 = partWhere((l) => l.isHorizontal || l.isVertical || l.isDiagonal);

d7575bd1466ef7fd889b9a7fdeb2ae109fc826c7
Pieter van Loon <git@pietervanloon.com>
2021-12-22 00:11:45 +0100

    Optimised day15 a bit further

--- day15/day15.dart
+++ day15/day15.dart
@@ -76,36 +76,42 @@ final part2 = Part(
           .map((row) => row.map(increment).map((i) => i == 10 ? 1 : i).toList())
           .toList());
     }
+    final width = input.first.length;
+    final height = input.first.length;
     input[0][0] = 0;
-    final costToReach = List.generate(
-        input.length, (_) => List.filled(input.first.length, maxInt64 ~/ 2));
-    final queued = List.generate(
-        input.length, (_) => List.filled(input.first.length, false));
-    costToReach[input.length - 1][input.first.length - 1] = input.last.last;
-    queued[input.length - 1][input.first.length - 1] = true;
+    final costToReach =
+        List.generate(height, (_) => List.filled(width, maxInt64 ~/ 2));
+    final queued = List.generate(height, (_) => List.filled(width, false));
+    costToReach[height - 1][width - 1] = input.last.last;
+    queued[height - 1][width - 1] = true;
     final toExplore = Queue.of([
-      Point(input.first.length - 1, input.length - 2),
-      Point(input.first.length - 2, input.length - 1),
+      Point(width - 1, height - 2),
+      Point(width - 2, height - 1),
     ]);
-    queued[input.length - 2][input.first.length - 1] = true;
-    queued[input.length - 1][input.first.length - 2] = true;
+    queued[height - 2][width - 1] = true;
+    queued[height - 1][width - 2] = true;
     while (toExplore.isNotEmpty) {
       final point = toExplore.removeFirst();
       queued[point.y][point.x] = false;
       final risk = input[point.y][point.x];
       int cost = maxInt64;
-      if (point.y < input.length - 1) {
-        cost = min(cost, risk + costToReach[point.y + 1][point.x]);
+      if (point.y < height - 1) {
+        final val = costToReach[point.y + 1][point.x];
+        if (val < cost) cost = val;
       }
       if (point.y > 0) {
-        cost = min(cost, risk + costToReach[point.y - 1][point.x]);
+        final val = costToReach[point.y - 1][point.x];
+        if (val < cost) cost = val;
       }
-      if (point.x < input.first.length - 1) {
-        cost = min(cost, risk + costToReach[point.y][point.x + 1]);
+      if (point.x < width - 1) {
+        final val = costToReach[point.y][point.x + 1];
+        if (val < cost) cost = val;
       }
       if (point.x > 0) {
-        cost = min(cost, risk + costToReach[point.y][point.x - 1]);
+        final val = costToReach[point.y][point.x - 1];
+        if (val < cost) cost = val;
       }
+      cost += risk;
       if (costToReach[point.y][point.x] > cost) {
         costToReach[point.y][point.x] = cost;
         if (point.x > 0 &&
@@ -120,13 +126,13 @@ final part2 = Part(
           queued[point.y - 1][point.x] = true;
           toExplore.add(Point(point.x, point.y - 1));
         }
-        if (point.x < input.first.length - 1 &&
+        if (point.x < width - 1 &&
             !queued[point.y][point.x + 1] &&
             cost < costToReach[point.y][point.x + 1]) {
           queued[point.y][point.x + 1] = true;
           toExplore.add(Point(point.x + 1, point.y));
         }
-        if (point.y < input.first.length - 1 &&
+        if (point.y < width - 1 &&
             !queued[point.x][point.y + 1] &&
             cost < costToReach[point.y + 1][point.x]) {
           queued[point.y + 1][point.x] = true;
